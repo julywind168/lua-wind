e35396eb1094c9f050b50a8bbc9104f3f3592ae9
windy <707298413@qq.com>
2023-07-11 20:00:04 +0800

    add service:call

--- service/test_httpclient.lua
+++ service/test_httpclient.lua
@@ -1,23 +1,32 @@
+local wind = require "lualib.wind"
+
 local TestHttpClient = {}
 
 
 function TestHttpClient:__init()
     -- test http.get
-    self:fetch("https://api.vvhan.com/api/joke", function (r)
-        self:log("get response:", r.body)
-    end)
+    -- self:fetch("https://api.vvhan.com/api/joke", function (r)
+    --     self:log("get response:", r.body)
+    -- end)
 
     -- test http.post
-    self:fetch(
-        "https://jsonplaceholder.typicode.com/posts",
-        {
-            method = "POST",
-            body = {useId = 1, nick = "windy"}
-        },
-        function (r)
-            self:log("post response:", r.body)
-        end
-    )
+    -- self:fetch(
+    --     "https://jsonplaceholder.typicode.com/posts",
+    --     {
+    --         method = "POST",
+    --         body = {useId = 1, nick = "windy"}
+    --     },
+    --     function (r)
+    --         self:log("post response:", r.body)
+    --     end
+    -- )
+end
+
+function TestHttpClient:joke()
+    local ret = wind.ret
+    self:fetch("https://api.vvhan.com/api/joke", function (r)
+        ret(r.body)
+    end)
 end
 
 
